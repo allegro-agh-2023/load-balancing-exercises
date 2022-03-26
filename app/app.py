34093373de5beb33ebfa1216113bb572a59ac142
Mateusz Kuzmik <mateusz.kuzmik@allegro.pl>
2022-03-26 16:59:56 +0100

    Application request id logging and sleeping to imitate timeouts

--- app/app.py
+++ app/app.py
@@ -1,7 +1,38 @@
+import random
+import string
+import time
 from flask import Flask
+from logging.config import dictConfig
+
+dictConfig({
+    'version': 1,
+    'formatters': {'default': {
+        'format': '[%(asctime)s] %(levelname)s: %(message)s',
+    }},
+    'handlers': {'wsgi': {
+        'class': 'logging.StreamHandler',
+        'stream': 'ext://sys.stdout',
+        'formatter': 'default'
+    }},
+    'root': {
+        'level': 'INFO',
+        'handlers': ['wsgi']
+    }
+})
 
 app = Flask(__name__)
 
+
+def generate_request_id():
+    length = 5
+    available_chars = string.ascii_uppercase + string.digits
+    return ''.join(random.choice(available_chars) for _ in range(length))
+
+
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    request_id = generate_request_id()
+    app.logger.info("[%s] Got request", request_id)
+    time.sleep(1)  # doing some job!
+    app.logger.info("[%s] Serving response", request_id)
+    return request_id
