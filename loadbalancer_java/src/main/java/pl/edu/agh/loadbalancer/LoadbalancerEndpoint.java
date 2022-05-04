47ad708fb5592095deb4baaf8119e204d66589dc
Mateusz Kuzmik <mateusz.kuzmik@allegro.pl>
2022-05-04 20:20:07 +0200

    loadbalancer java - pass request to single instance

--- loadbalancer_java/src/main/java/pl/edu/agh/loadbalancer/LoadbalancerEndpoint.java
+++ loadbalancer_java/src/main/java/pl/edu/agh/loadbalancer/LoadbalancerEndpoint.java
@@ -1,23 +1,30 @@
 package pl.edu.agh.loadbalancer;
 
-import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.client.RestTemplate;
 
-import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-import static org.springframework.web.bind.annotation.RequestMethod.HEAD;
-import static org.springframework.web.bind.annotation.RequestMethod.OPTIONS;
-import static org.springframework.web.bind.annotation.RequestMethod.PATCH;
-import static org.springframework.web.bind.annotation.RequestMethod.POST;
-import static org.springframework.web.bind.annotation.RequestMethod.PUT;
-import static org.springframework.web.bind.annotation.RequestMethod.TRACE;
+import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class LoadbalancerEndpoint {
-    @RequestMapping(value = "/**", method = {GET, HEAD, POST, PUT, PATCH, DELETE, OPTIONS, TRACE})
-    public ResponseEntity<String> defaultPath() {
-        return new ResponseEntity<String>("Unmapped request", HttpStatus.OK);
+
+    private final RestTemplate restTemplate;
+
+    public LoadbalancerEndpoint(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @RequestMapping(value = "/**")
+    public ResponseEntity<String> balanceLoad(HttpMethod httpMethod, @RequestBody(required = false) String requestBody,
+                                              @RequestHeader MultiValueMap<String, String> headers, HttpServletRequest request) {
+        String instanceUrl = "http://app-instance-1:5000";
+        return restTemplate.exchange(instanceUrl + request.getRequestURI(), httpMethod, new HttpEntity<>(requestBody, headers), String.class);
     }
 }
